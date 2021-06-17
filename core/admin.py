02704a6fbe05f6e332592f96f09e4bab34fa0de3
Sam <samuelkyama102@gmail.com>
2021-06-17 17:03:45 +0300

    added django admin registration

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from core.models import User, Country, Region, Competence, Occupation, Profile, \
+    ProfileRecommendation, Outbreak, ProfileDeployment
+
+admin.site.register(User)
+admin.site.register(Country)
+admin.site.register(Region)
+admin.site.register(Competence)
+admin.site.register(Occupation)
+admin.site.register(Profile)
+admin.site.register(ProfileRecommendation)
+admin.site.register(Outbreak)
+admin.site.register(ProfileDeployment)
