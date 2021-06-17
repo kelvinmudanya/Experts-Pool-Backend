762bb30c940656ef92ee23fdb886ce10e50481af
Sam <samuelkyama102@gmail.com>
2021-06-17 16:04:44 +0300

    added static files path

--- eac_rde_backend/settings.py
+++ eac_rde_backend/settings.py
@@ -130,5 +130,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
-
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
