6e524bd61ececcd137ca2d050200cde04454883e
Sam <samuelkyama102@gmail.com>
2021-06-17 16:05:34 +0300

    added static files path

--- eac_rde_backend/settings.py
+++ eac_rde_backend/settings.py
@@ -129,5 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR.joinpath('static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
